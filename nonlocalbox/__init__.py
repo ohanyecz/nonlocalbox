f965dfa26cf5c8f3f1291dd6a696958cf2509596
Ottó Hanyecz <hanyeczotto@gmail.com>
2022-12-13 11:39:03 +0100

    remove incomplete citation from __init__

--- nonlocalbox/__init__.py
+++ nonlocalbox/__init__.py
@@ -1,10 +1,2 @@
-# Bibtex entry detailing how to cite the package
-__citation__ = """@MISC{...,
-    author = {Ottó Hanyecz and Mátyás Koniorczyk and Péter Naszvadi},
-    title = {nonlocalbox: an API for simulating nonlocal no-signalling correlations in Python},
-    year = {2022},
-    url = {},
-}"""
-
 from .nonlocalbox import *
 from .version import __version__
